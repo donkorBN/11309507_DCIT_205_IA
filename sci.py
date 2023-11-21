f98ed2b8f2a1881fd337732d59cb2e48dae1e9f4
donkorBN <projectmanuel89@gmail.com>
2023-11-21 14:13:57 +0000

    homepage-commit

---
+++ sci.py
@@ -0,0 +1,11 @@
+class student(self):
+    pass
+
+class student(self, name, age, grade):  
+
+  def name_of (person):
+    self.name= name 
+    self.age = age
+    self.grade = grade  
+
+print(name_of.name(Kofi))
